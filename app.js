972266fa6e97e788ec786796471183995333c42e
jlaunit89 <75558272+jlaunit89@users.noreply.github.com>
2020-12-17 00:19:34 -0800

    added message prompts

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ while (enterpassword!=password) {
     enterpassword=prompt('Enter Password:','');
 }
 
-var Test = "What is the best Football Team?";
-getPrompt(Test);
+var promptQuestion = "What is the best football team?";
+var response = prompt(promptQuestion);
+console.log(response);
 
-function giveAlert(ans){
-    if(ans == "Patriots"){
-        alertmessage = "You are a winner too!"
-    } else {
-        alertMessage = "BOOOOO!"
-    }
+var alertMessage;
+var color;
+
+if (response === "Patriots") {
+    alertMessage = "You are a winner too!";
+    color = "#00FF00#"
+} else if (response === "patriots") {
+    alertMessage = "You are a winner too!";
+    color = "#FF0000";
+} else {
+    alertMessage = "BOOOOOOO!!"
+    color = "#FF0000"
 }
+
+alert(alertMessage);
+
+var element = document.getElementById('response-output')
+
+element.innerText = alertMessage;
+element.style.color = color;
